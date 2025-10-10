a8d96a78563173c8beb9fcdad446fcc602b21cc0
Herman Scheele <hermni31122@gmail.com>
2025-10-10 18:11:48 +0200

    p1b

--- mandatory_assign/main.py
+++ mandatory_assign/main.py
@@ -4,16 +4,45 @@ import pandas as pd
 
 df = pd.read_csv("finance_data.csv", sep=";")
 assets = df.columns.tolist()[1:]
+n = len(assets)
 
 
-# ------ Prob. 1 a) ------ #
-for a in assets:
-    p = df[a] # price data
-    r_hat = est_return(p)
-    sigma_hat = est_volatiliy(p)
+class Assignment:
 
-    print(f'Asset: {a}, r_hat: {r_hat}, sigma_hat: {sigma_hat}')    
-    plot_return_dist(p) # make normal fitted dist <<-----------------------------------
+    def p1a(self):
+        for a in assets:
+            p = df[a] # price data
+            r = get_returns(p) # returns for asset a
+
+            r_hat = est_return(r)
+            sigma_hat = est_volatiliy(r)
+
+            print(f'Asset: {a}, r_hat: {r_hat}, sigma_hat: {sigma_hat}')    
+            plot_return_dist(r, r_hat, sigma_hat)
 
 
-  
+    def p1b(self):
+        corrs = []
+        r_lst = []
+
+        for i in range(n):
+            r_i = get_returns(df[assets[i]])
+            r_lst.append(r_i)
+            for j in range(n):
+                r_j = get_returns(df[assets[j]])
+                c = corr(r_i, r_j)
+
+                print(f'Corr(R{i}, R{j}) = {c}')
+                
+        covar_matrix = cov_mat(r_lst)
+        print(covar_matrix)
+
+
+    def p1c(self):
+        return 0
+
+
+a = Assignment()
+
+# a.p1a()
+# a.p1b()
--- mandatory_assign/utils.py
+++ mandatory_assign/utils.py
@@ -5,65 +5,86 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-def est_return(price_data):
+def get_returns(price_data):
+    r = []
+    n = len(price_data)
 
-    n = len(price_data) 
-    r_sum = 0.0
     for i in range(n - 1):
         s_i = float(price_data[i].replace(',', "."))
         s_i_1 = float(price_data[i + 1].replace(',', "."))
+        r.append( (s_i_1 - s_i) / s_i )
 
-        r_i = (s_i_1 - s_i) / s_i
-        r_sum += r_i
-
-    r_k = r_sum / (n - 1) # n price points p, (n - 1) returns r
-    return r_k
+    return r
 
 
-def est_volatiliy(price_data):
+def est_return(r):
+    return sum(r) / len(r)
 
-    r_k = est_return(price_data)
-    n = len(price_data)
+
+def est_volatiliy(r):
+    n = len(r)
     s = 0.0
-    for i in range(n - 1):
-        s_i = float(price_data[i].replace(',', "."))
-        s_i_1 = float(price_data[i + 1].replace(',', "."))
-
-        r_i = (s_i_1 - s_i) / s_i
-        s += (r_i - r_k) ** 2
-
-    sigma = m.sqrt(s / (n - 2))
-    return sigma
-
-
-
-#--------------- need this for the fitted normal distribution -------------------- #
-def all_est_return_vol(data, df):
-    est_return_vol_list = []
-    for i in range(1, len(data)):
-        prices = df[data[i]]
-        t = (est_return(prices), est_volatiliy(prices))
-        est_return_vol_list.append(t)
-
-    return est_return_vol_list
-
-
-
-
-#----------- make and plot the fitted normal distribution --------------------#
-def plot_return_dist(price_data, r_k, sigma):
-
-    n = len(price_data)
-    r_is = []
-    for i in range(n - 1):
-        s_i = float(price_data[i].replace(',', "."))
-        s_i_1 = float(price_data[i + 1].replace(',', "."))
-
-        r_i = (s_i_1 - s_i) / s_i
-        r_is.append(r_i)
-
-    sns.histplot(r_is, color="skyblue")
+    for i in range(n):
+        s += (r[i] - est_return(r)) ** 2
     
+    return np.sqrt((1 / (n - 1)) * s)
+
+
+
+# #--------------- need this for the fitted normal distribution -------------------- #
+# def all_est_return_vol(data, df):
+#     est_return_vol_list = []
+#     for i in range(1, len(data)):
+#         prices = df[data[i]]
+#         t = (est_return(prices), est_volatiliy(prices))
+#         est_return_vol_list.append(t)
+
+#     return est_return_vol_list
+
+
+def fit_norm_dist(x, mu, sigma):
+    return (1 / np.sqrt(2*np.pi*sigma**2)) * np.exp(-(1/2) * ((x - mu)**2 / sigma**2))
+
+
+def plot_return_dist(r, mu, sigma):
+
+    x = np.linspace(mu - 4*sigma, mu + 4*sigma, 500)
+    y = fit_norm_dist(x, mu, sigma)
+
+    plt.plot(x, y, color='r', label='fitted normal dist.')
+    sns.histplot(r, color="skyblue", label='empirical dist.')
 
     plt.legend()
     plt.show()
+
+
+
+# Covariance
+def cov(r_i, r_j):
+
+    n = len(r_i)
+    s = 0.0
+    for k in range(n):
+        s += ( (r_i[k] - est_return(r_i)) * (r_j[k] - est_return(r_j)) )
+
+    return (1 / (n - 1)) * s
+
+
+# Correlation 
+def corr(r_i, r_j):
+    return cov(r_i, r_j) / (est_volatiliy(r_i) * est_volatiliy(r_j))
+
+
+# Covariance matrix
+def cov_mat(r):
+    n = len(r)
+    matrix = []
+    for i in range(n):
+        row = []
+        for j in range(n):
+            row.append(cov(r[i], r[j]))
+        matrix.append(row)
+
+    return np.array(matrix)
+
+
